cbaaaa7e6dbe6eead1df62f1a60f12971b72c764
sahasourav17 <sahasourav1170@gmail.com>
2023-02-13 23:59:20 +0600

    add invoice generate page

---
+++ component/generateInvoice/invoiceScript.js
@@ -0,0 +1,115 @@
+// loading inventory
+function loadInventory() {
+  let inventory = localStorage.getItem("inventory");
+  if (inventory) {
+    inventory = JSON.parse(inventory);
+    // console.log(inventory);
+    for (let i = 0; i < inventory.length; i++) {
+      //   addInventoryRow(inventory[i]);
+      if (inventory[i].quantity > 0) {
+        addInventoryRow(inventory[i]);
+      }
+    }
+  }
+  window.onscroll = function () {
+    window.scrollTo(0, 0);
+  };
+}
+
+function addInventoryRow(item) {
+  let table = document.getElementById("inventoryTable");
+  let row = table.insertRow();
+
+  let nameCell = row.insertCell(0);
+  let quantityCell = row.insertCell(1);
+  let priceCell = row.insertCell(2);
+  let actionsCell = row.insertCell(3);
+
+  nameCell.innerHTML = item.name;
+  quantityCell.innerHTML = item.quantity;
+  priceCell.innerHTML = item.price;
+  actionsCell.innerHTML = `
+    <button onclick="addToInvoice(this)">+</button>  
+    `;
+}
+
+let totalAmount = 0;
+function addToInvoice(button) {
+  let inventoryRowData = button.parentNode.parentNode;
+
+  // clicked item details
+  let itemName = inventoryRowData.cells[0].innerHTML;
+  let itemQuantity = inventoryRowData.cells[1].innerHTML;
+  let itemPrice = inventoryRowData.cells[2].innerHTML;
+
+  let qtyInput = prompt("Enter quantity");
+
+  invoiceTable = document.getElementById("invoiceTable");
+
+  let row = invoiceTable.insertRow();
+  let nameCell = row.insertCell(0);
+  let quantityCell = row.insertCell(1);
+  let priceCell = row.insertCell(2);
+
+  let price = qtyInput * itemPrice;
+  totalAmount += price;
+  //   console.log(price);
+
+  nameCell.innerHTML = itemName;
+  quantityCell.innerHTML = qtyInput;
+  priceCell.innerHTML = price;
+  //   console.log(totalAmount);
+
+  //   making changes to the inventory table wrt the invoice
+  let inventory = JSON.parse(localStorage.getItem("inventory"));
+
+  //   console.log("before", inventory);
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].name == nameCell.innerText) {
+      console.log(inventory[i].quantity);
+      inventory[i].quantity = inventory[i].quantity - qtyInput;
+    }
+  }
+
+  //   localStorage.removeItem("inventory");
+  console.log(inventory);
+
+  localStorage.setItem("inventory", JSON.stringify(inventory));
+  //   inventoryRowData.remove();
+
+  //   loadInventory();
+
+  // total payable amount
+  let amountPayable = document.getElementById("amountPayable");
+  amountPayable.innerHTML = `Total Amount to be paid: ${totalAmount}`;
+}
+
+function printInvoice() {
+  addSale(totalAmount, totalAmount * 0.1);
+  let divContents = document.getElementById("invoice-items").innerHTML;
+  let a = window.open("", "", "height=500, width=500");
+  a.document.write("<html><body style='text-align:center;'>");
+  a.document.write("<h3 >M/S Ananda Fashion</h3>");
+  a.document.write("<p>Dipa Mansion Market, Hajiganj, Chandpur</p>");
+  a.document.write(divContents);
+  a.document.write("</body></html>");
+  a.document.close();
+  a.print();
+}
+
+function addSale(total, profit) {
+  let date = new Date().toLocaleDateString();
+  let sale = {
+    date: date,
+    total: total,
+    profit: profit,
+  };
+  //   console.log(sale);
+
+  let sales = JSON.parse(localStorage.getItem("sales"));
+
+  sales.push(sale);
+  //   console.log(sales);
+
+  localStorage.setItem("sales", JSON.stringify(sales));
+}
--- script.js
+++ script.js
@@ -3,68 +3,68 @@ let inventory = [];
 
 // Function to load the inventory from local storage
 function loadInventory() {
-    // Check if there is any data in local storage
-    if (localStorage.getItem("inventory")) {
-        inventory = JSON.parse(localStorage.getItem("inventory"));
-        // Loop through the inventory items and add them to the table
-        for (let i = 0; i < inventory.length; i++) {
-            addInventoryRow(inventory[i]);
-        }
+  // Check if there is any data in local storage
+  if (localStorage.getItem("inventory")) {
+    inventory = JSON.parse(localStorage.getItem("inventory"));
+    // Loop through the inventory items and add them to the table
+    for (let i = 0; i < inventory.length; i++) {
+      addInventoryRow(inventory[i]);
     }
+  }
 }
 
 // Function to add an item to the inventory
 function addItem() {
-    // Get the values from the form inputs
-    let itemName = document.getElementById("itemName").value;
-    let itemQuantity = document.getElementById("itemQuantity").value;
-    let itemPrice = document.getElementById("itemPrice").value;
+  // Get the values from the form inputs
+  let itemName = document.getElementById("itemName").value;
+  let itemQuantity = document.getElementById("itemQuantity").value;
+  let itemPrice = document.getElementById("itemPrice").value;
 
-    if (itemName && itemPrice && itemQuantity){
-        // Create an object to store the item data 
-        let item = {
-            name: itemName,
-            quantity: itemQuantity,
-            price: itemPrice
-        };
+  if (itemName && itemPrice && itemQuantity > 0) {
+    // console.log(itemQuantity);
+    // Create an object to store the item data
+    let item = {
+      name: itemName,
+      quantity: itemQuantity,
+      price: itemPrice,
+    };
 
-        // Add the item to the inventory array
-        inventory.push(item);
+    // Add the item to the inventory array
+    inventory.push(item);
 
-        // Add the item to the table
-        addInventoryRow(item);
+    // Add the item to the table
+    addInventoryRow(item);
 
-        // Save the inventory to local storage
-        localStorage.setItem("inventory", JSON.stringify(inventory));
+    // Save the inventory to local storage
+    localStorage.setItem("inventory", JSON.stringify(inventory));
 
-        // Clear the form inputs
-        document.getElementById("itemName").value = "";
-        document.getElementById("itemQuantity").value = "";
-        document.getElementById("itemPrice").value = "";
-    }
-    else {
-        alert("Please fill all the fields")
-    }
+    // Clear the form inputs
+    document.getElementById("itemName").value = "";
+    document.getElementById("itemQuantity").value = "";
+    document.getElementById("itemPrice").value = "";
+  } else {
+    alert("Please fill all the fields");
+  }
 }
 
 // Function to add a row to the inventory table
 function addInventoryRow(item) {
-    // Get the inventory table
-    let table = document.getElementById("inventoryTable");
+  // Get the inventory table
+  let table = document.getElementById("inventoryTable");
 
-    // Create a new row
-    let row = table.insertRow();
+  // Create a new row
+  let row = table.insertRow();
 
-    // Insert the item data into the new row
-    let nameCell = row.insertCell(0);
-    let quantityCell = row.insertCell(1);
-    let priceCell = row.insertCell(2);
-    let actionsCell = row.insertCell(3);
+  // Insert the item data into the new row
+  let nameCell = row.insertCell(0);
+  let quantityCell = row.insertCell(1);
+  let priceCell = row.insertCell(2);
+  let actionsCell = row.insertCell(3);
 
-    nameCell.innerHTML = item.name;
-    quantityCell.innerHTML = item.quantity;
-    priceCell.innerHTML = item.price;
-    actionsCell.innerHTML = `
+  nameCell.innerHTML = item.name;
+  quantityCell.innerHTML = item.quantity;
+  priceCell.innerHTML = item.price;
+  actionsCell.innerHTML = `
     <button onclick="editItem(this)">Edit</button>  
     <button id = "del" onclick="deleteItem(this)">Delete</button>
     `;
@@ -72,81 +72,84 @@ function addInventoryRow(item) {
 
 // Function to edit an item
 function editItem(button) {
-    // Get the row that contains the item data
-    let row = button.parentNode.parentNode;
+  // Get the row that contains the item data
+  let row = button.parentNode.parentNode;
 
-    // Get the item data from the row
-    let itemName = row.cells[0].innerHTML;
-    let itemQuantity = row.cells[1].innerHTML;
-    let itemPrice = row.cells[2].innerHTML;
+  // Get the item data from the row
+  let itemName = row.cells[0].innerHTML;
+  let itemQuantity = row.cells[1].innerHTML;
+  let itemPrice = row.cells[2].innerHTML;
 
-    // Insert the item data into the form inputs
-    document.getElementById("itemName").value = itemName;
-    document.getElementById("itemQuantity").value = itemQuantity;
-    document.getElementById("itemPrice").value = itemPrice;
+  // Insert the item data into the form inputs
+  document.getElementById("itemName").value = itemName;
+  document.getElementById("itemQuantity").value = itemQuantity;
+  document.getElementById("itemPrice").value = itemPrice;
 
-    // Remove the item from the inventory array
-    let index = inventory.findIndex(i => i.name === itemName);
-    inventory.splice(index, 1);
+  // Remove the item from the inventory array
+  let index = inventory.findIndex((i) => i.name === itemName);
+  inventory.splice(index, 1);
 
-    // Remove the item from the table
-    row.remove();
+  // Remove the item from the table
+  row.remove();
 
-    // Save the inventory
-    localStorage.setItem("inventory", JSON.stringify(inventory));
+  // Save the inventory
+  localStorage.setItem("inventory", JSON.stringify(inventory));
 }
-
 
 // Function to delete an item
 function deleteItem(button) {
-    // Get the row that contains the item data
-    let row = button.parentNode.parentNode;
+  // Get the row that contains the item data
+  let row = button.parentNode.parentNode;
 
-    // Get the item name
-    let itemName = row.cells[0].innerHTML;
+  // Get the item name
+  let itemName = row.cells[0].innerHTML;
 
-    // Remove the item from the inventory array
-    let index = inventory.findIndex(i => i.name === itemName);
-    inventory.splice(index, 1);
+  // Remove the item from the inventory array
+  let index = inventory.findIndex((i) => i.name === itemName);
+  inventory.splice(index, 1);
 
-    // Remove the item from the table
-    row.remove();
+  // Remove the item from the table
+  row.remove();
 
-    // Save the inventory to local storage
-    localStorage.setItem("inventory", JSON.stringify(inventory));
+  // Save the inventory to local storage
+  localStorage.setItem("inventory", JSON.stringify(inventory));
 }
-
-
 
 // barcode scan
 
 function scanBarcode() {
-    Quagga.init({
+  Quagga.init(
+    {
       inputStream: {
         name: "Live",
         type: "LiveStream",
-        target: document.querySelector('#scanViewport')   
+        target: document.querySelector("#scanViewport"),
       },
       decoder: {
-        readers: ["code_128_reader"]
+        readers: ["code_128_reader"],
+      },
+    },
+    function (err) {
+      if (err) {
+        console.log(err);
+        return;
       }
-    }, function(err) {
-        if (err) { console.log(err); return }
-        Quagga.start();
-    });
+      Quagga.start();
+    }
+  );
 }
 
-Quagga.onDetected(function(result) {
-    let barcode = result.codeResult.code;
-    console.log(barcode);
-    //Add barcode to inventory and update the local storage
-    let item = {
-        name: barcode,
-        quantity: 1,
-        price: 0
-    };
-    inventory.push(item);
-    addInventoryRow(item);
-    localStorage.setItem("inventory", JSON.stringify(inventory));
-    Quagga.stop();
-  });
+Quagga.onDetected(function (result) {
+  let barcode = result.codeResult.code;
+  console.log(barcode);
+  //Add barcode to inventory and update the local storage
+  let item = {
+    name: barcode,
+    quantity: 1,
+    price: 0,
+  };
+  inventory.push(item);
+  addInventoryRow(item);
+  localStorage.setItem("inventory", JSON.stringify(inventory));
+  Quagga.stop();
+});
